8e3d7f810f95bcbee6566da06cf4b870e52dcb88
sls1005 <sls1005@github.com>
2024-02-22 22:37:00 +0800

    Change `int` to `int8`. Also change `let` to `var`, or it wouldn't be accepted by compiler prior to 2.0

--- tests/test3.nim
+++ tests/test3.nim
@@ -1,13 +1,11 @@
 import cppconst
 
-proc f(p: ptr CConst[int]): int =
+proc f(p: ptr CConst[int8]): int8 =
   return p.derefAsNonConst() + 1
 
 proc main =
-  let
-    x = int(10)
-    p1 = addr(x)
-
+  var x = int8(10)
+  let p1 = addr(x)
   doAssert f(p1) == 11
 
 main()
