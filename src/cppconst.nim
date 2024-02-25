c4c1bcfa5d65304987983c7b4cf125382d684704
sls1005 <sls1005@github.com>
2024-02-25 12:51:06 +0800

    Edit comment

--- src/cppconst.nim
+++ src/cppconst.nim
@@ -5,7 +5,7 @@ type CConst*[T] {.importcpp: "std::add_const<'0>::type", header: "<type_traits>"
   ## This can not be normally instantiated. It is NOT intended to be used directly as the type of a variable, but to be used to form a more complex type (usually a pointer).
   ## For example, `ptr CConst[cchar]` is corresponding to `const char*` (non-const pointer to a const char) in C++.
   ##
-  ## A variable whose type is in the form `CConst[T]`, where `T` can be any type, will (probably) cause an error at the C++ compile-time. (But it can be of the type `ptr CConst[T]`.)
+  ## A variable whose type is in the form `CConst[T]`, where `T` can be any type, will (probably) cause an error at the C++ compile-time. (But it can be of the type `ptr CConst[T]`.) The same applies to return types of procedures.
   ##
   ## **Example:**
   ##
@@ -16,6 +16,7 @@ type CConst*[T] {.importcpp: "std::add_const<'0>::type", header: "<type_traits>"
   ## **Note:** A `const char*` is a `ptr CConst[cchar]` (non-const pointer to const char), not `CConst[ptr cchar]` (const pointer to non-const char, represented as `char * const` in C++) nor `CConst[cstring]` (`char * const`).
 
 {.push importcpp: "(const_cast<'0>(#))", noconv, nodecl, raises: [].}
+# `const_cast` is used here as a fail-safe mechanism. If the input type weren't compatible with the (underlying) return type, the code wouldn't even pass the C++ compilation.
 
 proc toPtrToNonConst*[T](p: ptr CConst[T]): ptr T
   ## This is **unsafe** as it discards the const qualifier.
@@ -26,8 +27,6 @@ proc toCString*(p: ptr CConst[cchar]): cstring
   ## This is **unsafe** as it discards the const qualifier.
   ##
   ## Any character pointed to by a `cstring` returned by this should not be assigned to, unless it is provably safe to do so, or a segmentation fault, or something worse, can happen at the runtime.
-   #
-   # `const_cast` is used here as a fail-safe mechanism. If the input type weren't compatible with the (underlying) return type, the code wouldn't even pass the C++ compilation.
 
 proc toPtrToCConstImpl[T](p: ptr T): ptr CConst[T]
 
